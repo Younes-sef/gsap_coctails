5e4d7c0079c8b8ce7f3b1cadc003fcb05573d57d
younes-sef <y64nes@gmail.com>
2025-07-01 11:33:55 +0100

    first commit

---
+++ landing_page/src/App.jsx
@@ -0,0 +1,18 @@
+import gsap from "gsap"
+import {ScrollTrigger, SplitText} from "gsap/all"
+import NavBar from "./components/NavBar"
+import { Hero } from "./components/Hero"
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+function App() {
+
+  return (
+  <main>
+    <NavBar />
+    <Hero />
+  </main>
+  )
+}
+
+export default App
