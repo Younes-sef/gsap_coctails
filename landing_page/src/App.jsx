59a944ba5a70b80050fe2f3aba302a1bd7b62c81
younes-sef <y64nes@gmail.com>
2025-07-02 15:01:19 +0100

    Implement cocktails section

--- landing_page/src/App.jsx
+++ landing_page/src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap"
 import {ScrollTrigger, SplitText} from "gsap/all"
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import { Coctails } from "./components/Coctails"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -11,6 +12,7 @@ function App() {
   <main>
     <NavBar />
     <Hero />
+    <Coctails />
   </main>
   )
 }
