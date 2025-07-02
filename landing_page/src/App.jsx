8839eb5a94b8b907e1003e4e42ddc91e02919d15
younes-sef <y64nes@gmail.com>
2025-07-02 14:45:49 +0100

    Implement hero scroll animation

--- landing_page/src/App.jsx
+++ landing_page/src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap"
 import {ScrollTrigger, SplitText} from "gsap/all"
 import NavBar from "./components/NavBar"
-import { Hero } from "./components/Hero"
+import Hero from "./components/Hero"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
--- landing_page/src/components/Hero.jsx
+++ landing_page/src/components/Hero.jsx
@@ -4,10 +4,12 @@ import { SplitText } from "gsap/all";
 import { useRef } from "react";
 import { useMediaQuery } from "react-responsive";
 
-
-export const Hero = () => {
-
-     useGSAP(() => {
+const Hero = () => {
+ const videoRef = useRef();
+ 
+ const isMobile = useMediaQuery({ maxWidth: 767 });
+ 
+ useGSAP(() => {
 	const heroSplit = new SplitText(".title", {
 	 type: "chars, words",
 	});
@@ -25,7 +27,8 @@ export const Hero = () => {
 	 ease: "expo.out",
 	 stagger: 0.06,
 	});
-    gsap.from(paragraphSplit.lines, {
+	
+	gsap.from(paragraphSplit.lines, {
 	 opacity: 0,
 	 yPercent: 100,
 	 duration: 1.8,
@@ -33,8 +36,8 @@ export const Hero = () => {
 	 stagger: 0.06,
 	 delay: 1,
 	});
-    
-    gsap
+	
+	gsap
 	.timeline({
 	 scrollTrigger: {
 		trigger: "#hero",
@@ -45,9 +48,30 @@ export const Hero = () => {
 	})
 	.to(".right-leaf", { y: 200 }, 0)
 	.to(".left-leaf", { y: -200 }, 0)
-    }, []);
-    return(
-        <>
+	.to(".arrow", { y: 100 }, 0);
+	
+	const startValue = isMobile ? "top 50%" : "center 60%";
+	const endValue = isMobile ? "120% top" : "bottom top";
+	
+	let tl = gsap.timeline({
+	 scrollTrigger: {
+		trigger: "video",
+		start: startValue,
+		end: endValue,
+		scrub: true,
+		pin: true,
+	 },
+	});
+	
+	videoRef.current.onloadedmetadata = () => {
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+	};
+ }, []);
+ 
+ return (
+	<>
 	 <section id="hero" className="noisy">
 		<h1 className="title">MOJITO</h1>
 		
@@ -86,8 +110,16 @@ export const Hero = () => {
 	 </section>
 	 
 	 <div className="video absolute inset-0">
-		
+		<video
+		 ref={videoRef}
+		 muted
+		 playsInline
+		 preload="auto"
+		 src="/videos/input.mp4"
+		/>
 	 </div>
 	</>
-    )
-}
+ );
+};
+
+export default Hero;
